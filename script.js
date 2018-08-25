6a59ae59cf8c4781c4da9b97ab95f2cff69ccda6
JamesRileydev <jamesriley.dev@gmail.com>
2018-08-25 15:19:06 -0400

    more XML code

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = function () {
 
   var btn = this.document.querySelector('#btn');
-  var price = this.document.querySelector('currentPrice');
+  var priceDisp = this.document.querySelector('#price');
+  var currency = "USD";
 
   btn.addEventListener('click', function () {
 
@@ -9,7 +10,10 @@ window.onload = function () {
 
     XHR.onreadystatechange = function () {
       if (XHR.readyState == 4 && XHR.status == 200) {
-
+        var data = JSON.parse(XHR.responseText) 
+        var price = data.bpi[currency].rate;
+        priceDisp.innerText = currency + ' ' + price;
+        // console.log(XHR.responseText);
       }
     }
     var url = "https://api.coindesk.com/v1/bpi/currentprice.json"
