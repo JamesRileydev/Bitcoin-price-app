beaa3495081c8d104bf8cbe575c15651870b14c2
JamesRileydev <jamesriley.dev@gmail.com>
2018-08-25 15:32:36 -0400

    Adding radio buttons for currency

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ window.onload = function () {
   var currency = "USD";
 
   btn.addEventListener('click', function () {
+    getPrice();
+    console.log("Button Clicked!")
+  });
 
+  function getPrice() {
     var XHR = new XMLHttpRequest();
 
     XHR.onreadystatechange = function () {
       if (XHR.readyState == 4 && XHR.status == 200) {
-        var data = JSON.parse(XHR.responseText) 
+        var data = JSON.parse(XHR.responseText)
         var price = data.bpi[currency].rate;
         priceDisp.innerText = currency + ' ' + price;
         // console.log(XHR.responseText);
@@ -19,17 +23,8 @@ window.onload = function () {
     var url = "https://api.coindesk.com/v1/bpi/currentprice.json"
     XHR.open("GET", url);
     XHR.send();
+  }
 
-  });
-
-
-
-
-
-
-
-
-
-
+getPrice();
 
 }
